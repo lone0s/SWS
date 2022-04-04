9ef4bb1ead3d6c3a2e7e7d5de644b889bcd13482
BENMOUSSATI SOUHAIL <sbenmo01@sfasp2mi.univ-poitiers.fr>
2022-04-04 12:17:10 +0200

    controller forms

---
+++ src/Controller/FormController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FormController extends AbstractController
+{
+    #[Route('/', name: 'app_form')]
+}
