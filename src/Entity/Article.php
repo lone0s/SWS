ae62bd43c01bd786913716c58847132e7a140a3d
ohno <pestiference@gmail.com>
2022-04-10 17:54:43 +0200

    Generated Entities through Symfony

---
+++ src/Entity/Article.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Article
+ *
+ * @ORM\Table(name="im22_article")
+ * @ORM\Entity
+ */
+class Article
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="libelle", type="text", nullable=false)
+     */
+    private $libelle;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="price", type="float", precision=10, scale=0, nullable=false)
+     */
+    private $price;
+
+    /**
+     * @var int|null
+     *
+     * @ORM\Column(name="quantity", type="integer", nullable=true)
+     */
+    private $quantity;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLibelle(): ?string
+    {
+        return $this->libelle;
+    }
+
+    public function setLibelle(string $libelle): self
+    {
+        $this->libelle = $libelle;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getQuantity(): ?int
+    {
+        return $this->quantity;
+    }
+
+    public function setQuantity(?int $quantity): self
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+
+
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="im22_user")
+ * @ORM\Entity
+ */
+class User
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="login", type="string", length=255, nullable=false)
+     */
+    private $login;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="password", type="string", length=255, nullable=false)
+     */
+    private $password;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="firstname_lastname", type="string", length=50, nullable=false)
+     */
+    private $firstnameLastname;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="isAdmin", type="boolean", nullable=false)
+     */
+    private $isadmin;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="isSuperAdmin", type="boolean", nullable=false)
+     */
+    private $issuperadmin;
+
+    /**
+     * @var int|null
+     *
+     * @ORM\Column(name="id_article", type="integer", nullable=true)
+     */
+    private $idArticle;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLogin(): ?string
+    {
+        return $this->login;
+    }
+
+    public function setLogin(string $login): self
+    {
+        $this->login = $login;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getFirstnameLastname(): ?string
+    {
+        return $this->firstnameLastname;
+    }
+
+    public function setFirstnameLastname(string $firstnameLastname): self
+    {
+        $this->firstnameLastname = $firstnameLastname;
+
+        return $this;
+    }
+
+    public function getIsadmin(): ?bool
+    {
+        return $this->isadmin;
+    }
+
+    public function setIsadmin(bool $isadmin): self
+    {
+        $this->isadmin = $isadmin;
+
+        return $this;
+    }
+
+    public function getIssuperadmin(): ?bool
+    {
+        return $this->issuperadmin;
+    }
+
+    public function setIssuperadmin(bool $issuperadmin): self
+    {
+        $this->issuperadmin = $issuperadmin;
+
+        return $this;
+    }
+
+    public function getIdArticle(): ?int
+    {
+        return $this->idArticle;
+    }
+
+    public function setIdArticle(?int $idArticle): self
+    {
+        $this->idArticle = $idArticle;
+
+        return $this;
+    }
+
+
+}
