ff560b678b7aff0b1c6b69b906081fadb5e71065
unknown <cobrarive@gmail.com>
2022-04-21 16:44:55 +0200

    Edition user from

---
+++ src/Form/EditUserType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\AuthUser;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EditUserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('login')
+            ->add('roles')
+            ->add('password')
+            ->add('firstName')
+            ->add('lastName')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => AuthUser::class,
+        ]);
+    }
+}
