8e1c87818dfbcbc1ea7504a1240491a7f0bfd15f
unknown <cobrarive@gmail.com>
2022-04-26 11:43:55 +0200

    style page et autres

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,22 +2,24 @@
 
 namespace App\Controller;
 
+use App\Entity\AuthUser;
 use App\Form\EditProfilType;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/app', name: 'app')]
 class ProfileController extends AbstractController
 {
     #[Route('/editprofile', name: '_edit_profile')]
-    public function editProfileAction(ManagerRegistry $doc, Request $request): Response
+    public function editProfileAction(ManagerRegistry $doc, Request $request, UserPasswordHasherInterface $userPasswordHasher): Response
     {
         $em = $doc->getManager();
-        /** @var \App\Entity\AuthUser $user */
+        /** @var AuthUser $user */
         $user = $this->getUser();
         $form = $this->createForm(EditProfilType::class, $user);
         $form->add("send", SubmitType::class, ['label' => "Valider les changements"]);
@@ -25,6 +27,12 @@ class ProfileController extends AbstractController
 
         if ($form->isSubmitted() && $form -> isValid()) {
             $user = $form->getData();
+            $user->setPassword(
+                $userPasswordHasher->hashPassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
             $em->persist($user);
             $em->flush();
             $this->addFlash('info', "Modifications réussies");
