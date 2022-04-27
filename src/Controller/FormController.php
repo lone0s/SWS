e693e217efb981224e4cba9a795a10462ca4f143
unknown <cobrarive@gmail.com>
2022-04-27 13:07:08 +0200

    sauv

--- src/Controller/FormController.php
+++ src/Controller/FormController.php
@@ -11,28 +11,5 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class FormController extends AbstractController
 {
-    /*
-     * Authentification 1ere partie
-     */
-    // Pour pas avoir a redéfinir a chaque fois
-    /*public static array $userPrivileges = array(
-        'isadmin' => true,
-        'issuperadmin' => true,
-        'idArticle' => null
-    );*/
 
-    #[Route('/create_account', name: 'create_account')]
-    public function createAccountAction(EntityManager $em) : Response {
-        $authRepository = $em -> getRepository('App:User');
-        //$auth = new User();
-        $form = $this -> createForm(User::class);
-        $form -> add('send',SubmitType::class, ['label' => 'Creer un compte']);
-
-        $args = array('myform' => $form -> createView(), 'myUserPrivileges' => array(
-            'isadmin' => true,
-            'issuperadmin' => true,
-            'idArticle' => null));
-
-        return $this -> render('Form/formAuth.html.twig', $args);
-    }
 }
